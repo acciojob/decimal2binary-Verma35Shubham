f5d540d89af9637b4343fb5950589be2d2f1a190
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-14 17:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-function threeSum(arr, target) {
-  const triplets = [];
-  arr.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length - 2; i++) {
-    if (i === 0 || (i > 0 && arr[i] !== arr[i - 1])) {
-      let lo = i + 1;
-      let hi = arr.length - 1;
-      let sum = target - arr[i];
-      while (lo < hi) {
-        if (arr[lo] + arr[hi] === sum) {
-          triplets.push([arr[i], arr[lo], arr[hi]]);
-          while (lo < hi && arr[lo] === arr[lo + 1]) lo++;
-          while (lo < hi && arr[hi] === arr[hi - 1]) hi--;
-          lo++;
-          hi--;
-        } else if (arr[lo] + arr[hi] < sum) {
-          lo++;
-        } else {
-          hi--;
-        }
-      }
-    }
+function decimalToBinary(decimal) {
+  let binary = "";
+  while (decimal > 0) {
+    let remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
   }
-  return triplets;
+  return binary;
 }
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
