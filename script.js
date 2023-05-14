bf8eedb8345bcbca4ed99cc8a46ba7d3e307c806
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-14 17:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function threeSum(arr, target) {
-// write your code here
-  
+  const triplets = [];
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (i === 0 || (i > 0 && arr[i] !== arr[i - 1])) {
+      let lo = i + 1;
+      let hi = arr.length - 1;
+      let sum = target - arr[i];
+      while (lo < hi) {
+        if (arr[lo] + arr[hi] === sum) {
+          triplets.push([arr[i], arr[lo], arr[hi]]);
+          while (lo < hi && arr[lo] === arr[lo + 1]) lo++;
+          while (lo < hi && arr[hi] === arr[hi - 1]) hi--;
+          lo++;
+          hi--;
+        } else if (arr[lo] + arr[hi] < sum) {
+          lo++;
+        } else {
+          hi--;
+        }
+      }
+    }
+  }
+  return triplets;
 }
 
 module.exports = threeSum;
