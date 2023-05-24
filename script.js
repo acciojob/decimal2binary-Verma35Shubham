06e2e68b0270d3b0936349e5f0053cfd030addba
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-24 16:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function decimalToBinary(decimal) {
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
+  let binary = "";
   while (decimal > 0) {
-    binary = (decimal % 2) + binary;
+    let remainder = decimal % 2;
+    binary = remainder + binary;
     decimal = Math.floor(decimal / 2);
   }
-
   return binary;
 }
 
